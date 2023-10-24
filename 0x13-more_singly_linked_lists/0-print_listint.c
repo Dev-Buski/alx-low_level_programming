37963760b72b6ae346ed6abb8d2dde9b7c66d195
DevBuski <adebusolaayoolaoye@gmail.com>
2023-10-23 19:31:18 -0700

    SLL2

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,14 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
+	if (h != NULL)
+	{
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		count++;
+	}
 	}
 	return (count);
 }
